913b3175e246d782ef05bad6fd1362fad32beaa3
Tobias Hienzsch <tobias@ltaudio.de>
2022-06-04 02:29:37 +0200

    [dsp] Add docs to FourierBin

--- src/lt_dsp/fft/FourierBin.hpp
+++ src/lt_dsp/fft/FourierBin.hpp
@@ -6,21 +6,33 @@
 namespace lt
 {
 
+/// \brief Returns the centre frequency of a fft bin.
 template<typename T>
 auto frequencyForBin(std::size_t binIndex, std::size_t fftSize, double sampleRate) -> T
 {
     return static_cast<T>(binIndex) * static_cast<T>(sampleRate) / static_cast<T>(fftSize);
 }
 
+/// \brief Bundles the fft bin frequency with its complex value.
 template<typename T>
 struct FourierBin
 {
+    /// \brief Constructs an empty FourierBin.
     FourierBin() noexcept = default;
+
+    /// \brief Constructs a FourierBin with the given frequency and complex value.
     FourierBin(T frequency, std::complex<T> value) noexcept;
 
+    /// \brief Returns the complex value of this bin.
     [[nodiscard]] auto value() const noexcept -> std::complex<T>;
+
+    /// \brief Returns the centre frequency of this bin.
     [[nodiscard]] auto frequency() const noexcept -> T;
+
+    /// \brief Returns the magnitude of this bin.
     [[nodiscard]] auto magnitude() const noexcept -> T;
+
+    /// \brief Returns the phase in radians of this bin.
     [[nodiscard]] auto phase() const noexcept -> T;
 
 private:
@@ -48,15 +60,13 @@ auto FourierBin<T>::frequency() const noexcept -> T
 template<typename T>
 auto FourierBin<T>::magnitude() const noexcept -> T
 {
-    auto const r = real(_value);
-    auto const i = imag(_value);
-    return std::sqrt(r * r + i * i);
+    return std::abs(value());
 }
 
 template<typename T>
 auto FourierBin<T>::phase() const noexcept -> T
 {
-    return std::atan2(imag(_value), real(_value));
+    return std::arg(value());
 }
 
 }  // namespace lt
